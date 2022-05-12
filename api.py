7517758f5ff4820390d359ee03dd7be6bd32f15a
Mroik <mroik@delayed.space>
2022-05-12 15:59:42 +0200

    Add items methods and type hinting

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
 import json
+from typing import Union
 
 from requests import Session
 
@@ -11,6 +12,8 @@ def flush_session():
     global session
     session = Session()
 
+
+# Items
 
 class Item:
     """Superclass used to rappresent hackernews items"""
@@ -26,11 +29,16 @@ class Item:
 
 
 class Story(Item):
-    pass
+    def get_comments(self, start: int = 0, n: int = 10) -> list['Comment']:
+        return [get_item(id) for id in self.kids[start:start + n]]
 
 
 class Comment(Item):
-    pass
+    def get_replies(self, start: int = 0, n: int = 10) -> list['Comment']:
+        return [get_item(id) for id in self.kids[start:start + n]]
+
+    def get_parent(self) -> Union[Story, 'Comment']:
+        return get_item(self.parent)
 
 
 class Job(Item):
@@ -38,14 +46,19 @@ class Job(Item):
 
 
 class Poll(Item):
-    pass
+    def get_comments(self, start: int = 0, n: int = 10) -> list[Comment]:
+        return [get_item(id) for id in self.kids[start:start + n]]
+
+    def get_options(self) -> list['PollOpt']:
+        return [get_item(id) for id in self.parts]
 
 
 class PollOpt(Item):
-    pass
+    def get_poll(self) -> Poll:
+        return get_item(self.poll)
 
 
-def from_data(data: str) -> Item:
+def from_item_data(data: str) -> Item:
     """
     Helper function to get an Item object
 
@@ -68,4 +81,22 @@ def from_data(data: str) -> Item:
 def get_item(id: int) -> Item:
     """Return a Hackernews item"""
     data = session.get(ENDPOINT + f"item/{id}.json").text
-    return from_data(data)
+    return from_item_data(data)
+
+
+# Users
+
+class User:
+    def __init__(self, data: str = None):
+        if data is not None:
+            self.parse(data)
+
+    def parse(self, data: str):
+        data = json.loads(data)
+        for k in data:
+            self.__setattr__(k, data[k])
+
+    def from_id(id: str) -> 'User':
+        data = session.get(ENDPOINT + f"user/{id}.json").text
+        return User(data)
+
